10e07fdb3a1664887eb5a099968879dcc64fddf6
Razz700 <160166041+Razz700@users.noreply.github.com>
2024-04-16 09:44:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 function firstChar(text) {
   // your code here
 	let str=text.trim();
+	if(str==""){return str;}
 	return str[0];
 
 }
 
 // Do not change the code below
 
-const text = prompt("Enter text:");
-alert(firstChar(text));
+// const text = prompt("Enter text:");
+// alert(firstChar(text));
